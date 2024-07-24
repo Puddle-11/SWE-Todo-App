c72785ed3ee97b222e5c56d28b332644c6a6a626
Zer0Mortal <anbrockway615@gmail.com>
2024-07-24 14:58:37 -0400

    display stuff

--- SWE Todo App/TodoList.h
+++ SWE Todo App/TodoList.h
@@ -1,11 +1,28 @@
 #pragma once
 #include <vector>
+#include <string>
+#include <fstream>
 #include <iostream>
 class TodoList
 {
 public:
 	TodoList();
 	~TodoList();
+
+	std::vector<std::string> mainList;
+
+	void DisplayList()
+	{
+		for (int index = 0; index < mainList.size(); index++)
+		{
+			std::cout << "- " << mainList[index] << std::endl;
+		}
+	}
+
+	void SaveList()
+	{
+		
+	}
 
 private:
 
