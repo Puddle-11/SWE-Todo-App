0aae0c254367def835b4ba5c7ccf05452eb0b89f
Puddle <Puddleglumstudio@gmail.com>
2024-07-24 14:58:09 -0400

    Added get's and sets for mainList, added IsValidIndex to check if an index is within the range of the mainList

--- SWE Todo App/TodoList.cpp
+++ SWE Todo App/TodoList.cpp
@@ -6,3 +6,27 @@ TodoList::TodoList() {
 TodoList::~TodoList() {
 
 }
+
+std::vector<std::string> TodoList::GetTodoList() {
+	return mainList;
+}
+void TodoList::SetTodoList(std::vector<std::string> _input) {
+	mainList = _input;
+}
+
+bool TodoList::SetIndex(std::string _input, int _index) {
+	if (!IsValidIndex(_index)) return;
+
+	mainList[_index] = _input;
+}
+std::string TodoList::GetIndex(int _index) {
+	if (!IsValidIndex(_index)) return "";
+
+	return mainList[_index];
+}
+bool TodoList::IsValidIndex(int _index) {
+
+	if (_index < 0 || _index >= mainList.size()) return false;
+	return true;
+
+}
--- SWE Todo App/TodoList.h
+++ SWE Todo App/TodoList.h
@@ -6,8 +6,12 @@ class TodoList
 public:
 	TodoList();
 	~TodoList();
-
+	std::vector<std::string> GetTodoList();
+	void SetTodoList(std::vector<std::string> _input);
+	bool SetIndex(std::string _input, int _index);
+	std::string GetIndex(int _index);
 private:
+	bool IsValidIndex(int _index);
 	std::vector < std::string> mainList;
 };
 
