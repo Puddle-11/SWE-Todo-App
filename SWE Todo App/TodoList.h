f027d392ed8f3e5c7b163a84c1cb5e7e0531b086
Zer0Mortal <anbrockway615@gmail.com>
2024-07-24 15:20:10 -0400

    added methods to save and load list

--- SWE Todo App/TodoList.h
+++ SWE Todo App/TodoList.h
@@ -7,6 +7,49 @@ public:
 	TodoList();
 	~TodoList();
 
+	std::vector<std::string> mainList;
+
+	void DisplayList()
+	{
+		for (int index = 0; index < mainList.size(); index++)
+		{
+			std::cout << "- " << mainList[index] << std::endl;
+		}
+	}
+
+	void SaveList()
+	{
+		std::ofstream file("list.txt");
+
+		if (file.is_open())
+		{
+			for (int index = 0; index < mainList.size(); index++)
+			{
+				file << mainList[index];
+			}
+
+			file.close();
+		}
+	}
+
+	void LoadList()
+	{
+		std::ifstream file("list.txt");
+		std::string reader = "";
+
+		if (file.is_open())
+		{
+			while (!file.eof())
+			{
+				getline(file, reader);
+
+				mainList.push_back("- " + reader);
+			}
+
+			file.close();
+		}
+	}
+
 private:
 
 };
